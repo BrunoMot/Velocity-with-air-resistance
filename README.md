8ad8c61c4acd7e1ae4dd5f6ead3bf696be2f949c
Bruno <bmarizot@gmail.com>
2019-06-05 15:28:25 -0300

    Update README.md

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 This repository contains the source files used to create graphs of the velocity of bodies of different masses in free fall
 
 Gif: https://www.reddit.com/r/dataisbeautiful/comments/bx5trc/oc_velocity_in_free_fall_for_different_inicial/
+
+__air_res.py:__ program for printing the graphs for the inicial velocities of 0 through 1000. __NOTE:__ running this program will create 13 png files, so you might want to adjust it or run it in a specific folder
+
+__air_vi_all.py:__ program for printing the plot that contains all inicial velocities.
---
+++ air_res.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors as mcolors
+
+'''
+F = g - Ar
+Ar = aV
+
+F = g - aV
+
+v' = (g - av)/m
+
+
+'''
+
+def solver(g, a, m, dt, T, vi):
+    dt = float(dt)
+    Nt = round(int(T/dt))
+    u = np.zeros(Nt+1)
+    t = np.linspace(0, Nt*dt, Nt + 1)
+
+    u[0] = vi
+
+    for n in range(0, Nt ):
+        u[n + 1] = (((g*dt)) + (u[n])*(1 - (a*dt)/(2*m)))/(1 + (a*dt)/(2*m))
+
+    return u, t
+
+
+def creategraph(vi, label):
+    g = 9.8
+    a = 2
+    T = 500
+    dt = 0.01
+
+    m = 1      
+    u, t = solver(g, a, m, dt, T, vi)
+    plt.plot(t, u, 'C0-')
+
+    m = 5      
+    u, t = solver(g, a, m, dt, T, vi)
+    plt.plot(t, u, 'C1-')
+
+    m = 20      
+    u, t = solver(g, a, m, dt, T, vi)
+    plt.plot(t, u, 'C2-')
+
+    m = 50      
+    u, t = solver(g, a, m, dt, T, vi)
+    plt.plot(t, u, 'C3-')
+
+    m = 100      
+    u, t = solver(g, a, m, dt, T, vi)
+    plt.plot(t, u, 'C4-')
+
+    umin = 0; umax = 1.2*u.max()
+    name = 'ar_vi' + str(vi)
+    plt.title('Velocity in free fall with air resistance\na=%g, vi=%g' % (a, vi) )
+    plt.legend(['m=1', 'm=5', 'm=20', 'm=50', 'm=100'], loc='upper right')
+    plt.xlabel('t')
+    plt.ylabel('v')
+    plt.axis([t[0], t[-1], umin, umax])
+    if vi == 1000:
+        label = 'e' + label
+    plt.savefig(label + name + '.png')
+
+for x in range(1, 11):
+    creategraph(100*x, 'b' + str(x))
+    plt.close('all')
+
+for x in [0, 10, 50]:
+    creategraph(x, 'a' + str(x))
+    plt.close('all')
